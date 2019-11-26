37e661f6cb2402534ae422a3fc721492d1c6c3c0
Yulia Beton <yuliabeton@gmail.com>
2019-11-27 00:23:47 +0300

    fix3

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@
 
 import readlineSync from 'readline-sync';
 
-export const userName = () => {
+export default () => {
+    console.log('Welcome to the Brain Games!');
     const name = readlineSync.question('May I have your name? ');
+    console.log(`Hello, ${name}!`);
 };
